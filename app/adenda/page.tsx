cf8335351d07ce7282c21acfc1233d0ed8ce0072
Her bunheng <hengnun68@gmail.com>
2026-02-11 17:03:44 +0700

    create route

---
+++ app/adenda/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+export default function Agenda() {
+  return (
+    <>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde molestiae
+        perferendis voluptates laudantium, cupiditate quidem. Reprehenderit
+        quasi est obcaecati harum repellendus numquam illum in quisquam officia
+        illo. Velit, provident minus.
+      </p>
+    </>
+  );
+}

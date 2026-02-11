1086239e34a155830888dd6675684782faaa3c6a
Her bunheng <hengnun68@gmail.com>
2026-02-11 20:58:16 +0700

    commit

--- app/adenda/page.tsx
+++ app/adenda/page.tsx
@@ -1,13 +1,58 @@
 "use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 export default function Agenda() {
+  const router = useRouter();
+
   return (
-    <>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde molestiae
-        perferendis voluptates laudantium, cupiditate quidem. Reprehenderit
-        quasi est obcaecati harum repellendus numquam illum in quisquam officia
-        illo. Velit, provident minus.
-      </p>
-    </>
+    <div className="relative min-h-screen bg-black">
+
+      {/* Back Button Top-Left */}
+      <button
+        onClick={() => router.back()}
+        className="absolute top-4 left-4 w-10 h-10 flex items-center justify-center
+                   bg-white hover:bg-white/80 text-white rounded-full shadow-lg
+                   transition-all duration-300 z-50"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+          color="#095693"
+        >
+          <path
+            fillRule="evenodd"
+            d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {/* Page Content */}
+      <div className="flex flex-col items-center justify-center min-h-screen py-8">
+
+        {/* Image */}
+        <div className="relative w-full max-w-5xl h-[50vh] md:h-[60vh] lg:h-[70vh] mb-10 sm:mb-6">
+          <Image
+            src="/2Desktop.png"
+            alt="desktop"
+            fill
+            className="object-contain lg:block hidden"
+            priority
+          />
+          <Image
+            src="/2Mobile.png"
+            alt="mobile"
+            fill
+            className="object-contain lg:hidden block"
+            priority
+          />
+        </div>
+
+      </div>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita rem minima quidem earum deleniti distinctio, ad, accusantium dignissimos pariatur sit sapiente esse. Sit ea pariatur deleniti illum aliquid fugit. Consectetur.
+    <div className="flex flex-col items-center justify-center animate-scale-up min-h-screen bg-black py-8">
+
+      {/* Image */}
+      <div className="relative w-full max-w-5xl h-[50vh] md:h-[60vh] lg:h-[70vh] mb-10 sm:mb-6">
+        <Image
+          src="/1Desktop.png"
+          alt="desktop"
+          fill
+          className="object-contain lg:block hidden"
+          priority
+        />
+        <Image
+          src="/1Mobile.png"
+          alt="mobile"
+          fill
+          className="object-contain lg:hidden block"
+          priority
+        />
+      </div>
+
+      {/* Button */}
+      <Link href="/adenda">
+        <button className="btn">
+          <span className="shine"></span>
+          <span className="pulse-text">View Agenda</span>
+        </button>
+      </Link>
+
     </div>
   );
 }

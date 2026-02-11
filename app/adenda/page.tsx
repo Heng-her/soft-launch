2f0fe4a83ddca29342936e08a235ccca10020199
Her bunheng <hengnun68@gmail.com>
2026-02-11 23:08:48 +0700

    update button - background image

--- app/adenda/page.tsx
+++ app/adenda/page.tsx
@@ -7,21 +7,34 @@ export default function Agenda() {
   const router = useRouter();
 
   return (
-    <div className="relative min-h-screen bg-black">
+    <div className="relative min-h-screen overflow-hidden">
+
+      {/* Background GIF */}
+      <div
+        className="absolute inset-0 -z-20"
+        style={{
+          backgroundImage: "url('/bg/adenda.gif')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+
+      {/* Blue Overlay */}
+      <div className="absolute inset-0 bg-[#0b3c66]/70 -z-10" />
 
       {/* Back Button Top-Left */}
       <button
         onClick={() => router.back()}
         className="absolute top-4 left-4 w-10 h-10 flex items-center justify-center
-                   bg-white hover:bg-white/80 text-white rounded-full shadow-lg
-                   transition-all duration-300 z-50"
+                 bg-white hover:bg-white/80 rounded-full shadow-lg
+                 transition-all duration-300 z-50"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="w-6 h-6"
-          color="#095693"
+          className="w-6 h-6 text-[#095693]"
         >
           <path
             fillRule="evenodd"
@@ -32,9 +45,7 @@ export default function Agenda() {
       </button>
 
       {/* Page Content */}
-      <div className="flex flex-col items-center justify-center min-h-screen py-8">
-
-        {/* Image */}
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen py-8">
         <div className="relative w-full max-w-5xl h-[50vh] md:h-[60vh] lg:h-[70vh] mb-10 sm:mb-6">
           <Image
             src="/2Desktop.png"
@@ -51,8 +62,9 @@ export default function Agenda() {
             priority
           />
         </div>
-
       </div>
+
     </div>
   );
+
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,52 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center animate-scale-up min-h-screen bg-black py-8">
+    <div className="relative min-h-screen overflow-hidden">
 
-      {/* Image */}
-      <div className="relative w-full max-w-5xl h-[50vh] md:h-[60vh] lg:h-[70vh] mb-10 sm:mb-6">
-        <Image
-          src="/1Desktop.png"
-          alt="desktop"
-          fill
-          className="object-contain lg:block hidden"
-          priority
-        />
-        <Image
-          src="/1Mobile.png"
-          alt="mobile"
-          fill
-          className="object-contain lg:hidden block"
-          priority
-        />
+      {/* Background GIF */}
+      <div
+        className="absolute inset-0 -z-20"
+        style={{
+          backgroundImage: "url('/bg/home.gif')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+
+      {/* Blue Overlay */}
+      <div className="absolute inset-0 bg-[#0b3c66]/60 -z-10" />
+
+      {/* Animated Content */}
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen py-8 animate-scale-up">
+
+        {/* Image */}
+        <div className="relative w-full max-w-5xl h-[50vh] md:h-[60vh] lg:h-[70vh] mb-10 sm:mb-6">
+          <Image
+            src="/1Desktop.png"
+            alt="desktop"
+            fill
+            className="object-contain lg:block hidden"
+            priority
+          />
+          <Image
+            src="/1Mobile.png"
+            alt="mobile"
+            fill
+            className="object-contain lg:hidden block"
+            priority
+          />
+        </div>
+
+        {/* Button */}
+        <Link href="/adenda">
+          <button className="btn">
+            <span className="shine"></span>
+            <span className="pulse-text">View Agenda</span>
+          </button>
+        </Link>
       </div>
-
-      {/* Button */}
-      <Link href="/adenda">
-        <button className="btn">
-          <span className="shine"></span>
-          <span className="pulse-text">View Agenda</span>
-        </button>
-      </Link>
-
     </div>
+
   );
 }

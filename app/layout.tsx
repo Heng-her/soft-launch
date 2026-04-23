0f9ad3180baddf87ae4e44354a581f86139298c8
Her bunheng <hengnun68@gmail.com>
2026-04-23 16:23:42 +0700

    fig bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "48hr Hotel Booking - Soft Launch",
-  description: "Experience the unforgettable stay with 48hr Hotel Booking. Join us for our soft launch and be the first to explore our exclusive offers and seamless booking experience. Don't miss out on the chance to turn unsold rooms into unforgettable stays!",
+  description:
+    "Experience the unforgettable stay with 48hr Hotel Booking. Join us for our soft launch and be the first to explore our exclusive offers and seamless booking experience. Don't miss out on the chance to turn unsold rooms into unforgettable stays!",
 };
 
 export default function RootLayout({
@@ -25,17 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <head>
-        {/* Preload images for home page */}
-        <link rel="preload" href="/STC_logo.svg" as="image" fetchPriority="high" />
-        <link rel="preload" href="/48hr_logo.svg" as="image" fetchPriority="high" />
-        {/* Preload agenda images used on first navigation to /adenda */}
-        <link rel="preload" href="/3Desktop.png" as="image" media="(min-width: 1024px)" fetchPriority="high" />
-        <link rel="preload" href="/3Mobile.png" as="image" media="(max-width: 1023px)" fetchPriority="high" />
-        {/* Preload custom font used on the home hero (OdenaGlamour) */}
-        <link rel="preload" href="/font/OdenaGlamour-BF6625f66aead7f.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
-      </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
